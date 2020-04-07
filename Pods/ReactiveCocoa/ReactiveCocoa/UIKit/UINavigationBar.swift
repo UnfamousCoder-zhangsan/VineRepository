355aab5862cc7b5ea3509dc0e723dc38b1adbca9
lsircode <1546294949@qq.com>
2020-04-08 00:13:43 +0800

    初始化

--- Pods/ReactiveCocoa/ReactiveCocoa/UIKit/UINavigationBar.swift
+++
@@ -1,9 +0,0 @@
-import ReactiveSwift
-import UIKit
-
-extension Reactive where Base: UINavigationBar {
-	/// Sets the barTintColor of the navigation bar.
-	public var barTintColor: BindingTarget<UIColor?> {
-		return makeBindingTarget { $0.barTintColor = $1 }
-	}
-}
--- Pods/Target Support Files/Pods-Vines/Pods-Vines-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Vines : NSObject
-@end
-@implementation PodsDummy_Pods_Vines
-@end
